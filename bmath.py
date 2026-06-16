95dd986f0e67187aa4738b4a14939942e9d92631
Ali Hassaan <hassaan.ali7776@gmail.com>
2026-06-16 23:59:32 +0500

    Basic Math syntax for Python

---
+++ bmath.py
@@ -0,0 +1,36 @@
+#day 2 of learning python how operators work
+
+'''print(5+2)
+print(2**3)
+print(10//4) #whole divide
+print(10/2)# divide by 2
+
+#we can also this
+x=5
+x+=3
+print(x)'''
+
+
+'''#we can directly print any statemet without having an if
+print(5>3)#answer will be true
+print (1>3)#so we can print any statemet using direct print
+x=3
+print(x>5)
+
+y=7
+print(y>x)#answer is true'''
+
+
+#many conitions on one loop
+age =30
+print(age>25 and age<=50)#answer will be true
+
+#some deep concepts
+print(5==5)
+print(5 is 5)# both work in same way
+
+#Python follows math rules:1.()2.**  3.* / // % 4.+ -
+print(2+3*5)#17
+
+
+
